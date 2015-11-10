03c8d7103478f0f010400c9d5b66d0bda16c7a9e
Wizongod <plasmaseer@gmail.com>
2015-11-10 16:17:49 +0000

    Fixed a bug in the training back-prop algorithm that resulted in much slower convergence.

--- TrainNN.m
+++ TrainNN.m
@@ -27,11 +27,10 @@ for cycle = 1:size(input_set,1)
     
     for L = NN.layers-1:-1:1
         dy_dxwb = dReLU(NN.x(:,:,L),NN.w(:,:,L),NN.b(:,:,L));
-        %dy_dxwb = dy_dxwb.*([ones(1,NN.structure(L+1)) zeros(1,max(NN.structure)-NN.structure(L+1))]);
         dy_dw = NN.x(:,:,L)'*dy_dxwb;
         w_new(:,:,L) = NN.w(:,:,L) + learn_rate*(dy_dw).*repmat(derr_gb,size(NN.w(:,:,L),1),1);
         b_new(:,:,L) = NN.b(:,:,L) + learn_rate*(dy_dxwb).*derr_gb;
-        derr_gb = derr_gb(1)*dy_dxwb(1)*NN.w(:,1,L)';
+        derr_gb = (NN.w(:,:,L)*(derr_gb.*dy_dxwb)')';
     end
     
     NN.w = w_new;
@@ -41,25 +40,6 @@ for cycle = 1:size(input_set,1)
         prev_toc = toc;
         fprintf('%.2fsecs : %d cycles have passed in this batch\n',toc,cycle)
     end
-    
-    %%%%%% Old code %%%%%%
-
-%     for j = size(NN.structure,2):-1:2
-%         for i = NN.structure(j):-1:1
-%             if j == size(NN.structure,2)
-%                 glob_grad = NN_output(i) - O(i);
-%             end
-% 
-%             for k = 1:NN.structure(j-1)
-%                 grad = glob_grad * dReLU(x(:,j-1),[],[]) * x(k,(j-1));
-%                 w_new(i,j,k) = w(i,j,k) - learn_rate * grad;
-%             end
-%             
-%             grad = glob_grad * dReLU(x(:,j-1),[],[]);
-%             b_new(i,j) = b(i,j) - learn_rate * grad;
-%         end
-%         glob_grad = glob_grad * dReLU(x(:,j-1),[],[]) * w(1,j,1);
-%     end
 
 end
 
